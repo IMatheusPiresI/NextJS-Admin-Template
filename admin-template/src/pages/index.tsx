e175d73d535110f4c222b3bd4b82e0f3285b5369
imatheuspiresi <matheuspdsousa@gmail.com>
2022-06-07 09:04:12 -0300

    Create Template using,NEXTJS, TAILWINDCSS, TYPESCRIPT

---
+++ admin-template/src/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="w-full h-screen bg-blue-500 flex justify-center items-center">
+        <h1 className="text-5xl text-white font-extrabold">Template</h1>
+    </div>
+  )
+}
