e732137af2058280b678f7dc1db2a6507321fe4a
imatheuspiresi <matheuspdsousa@gmail.com>
2022-06-21 17:44:03 -0300

    correção botão de confirmar edição do card

--- admin-template/src/components/topics/Card.tsx
+++ admin-template/src/components/topics/Card.tsx
@@ -135,7 +135,7 @@ export default function Card({title, image, content, id, uid}: CardProps) {
                         onChange={(e) => setNewContent(e.target.value)}
                         className={'h-40 text-sm'}
                     />
-                    <button type="submit" className="bg-gray-700 text-gray-200 py-2 px-6 absolute -bottom-14 rounded-lg shadow-lg shadow-black/50">Enviar</button>
+                    <button type="submit" className="bg-gray-700 text-gray-200 py-2 px-6 absolute -bottom-7 rounded-lg shadow-lg shadow-black/50">Enviar</button>
                 </form>
             )}
         </div>
