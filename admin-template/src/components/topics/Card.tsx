df419c62165bb18e615e4b14bc988a1fbd39852a
imatheuspiresi <matheuspdsousa@gmail.com>
2022-06-22 18:10:01 -0300

    correção min caracters

--- admin-template/src/components/topics/Card.tsx
+++ admin-template/src/components/topics/Card.tsx
@@ -136,7 +136,11 @@ export default function Card({title, image, content, id, uid}: CardProps) {
                         onChange={(e) => setNewContent(e.target.value)}
                         className={'h-40 text-sm'}
                     />
-                    <button type="submit" className="bg-gray-700 text-gray-200 py-2 px-6 absolute -bottom-7 rounded-lg shadow-lg shadow-black/50">Enviar</button>
+                    <button 
+                        type="submit" 
+                        className="bg-gray-700 text-gray-200 py-2 px-6 absolute -bottom-7 rounded-lg shadow-lg shadow-black/50"
+                        disabled={newContent.length < 15}
+                        >Enviar</button>
                 </form>
             )}
         </div>
